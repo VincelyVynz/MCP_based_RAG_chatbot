9b5dba00adc9817c724a10e3d06b0cc768db336f
Vynz <76029153+VincelyVynz@users.noreply.github.com>
2026-02-02 16:02:30 +0530

    Write retrieve_context function

--- server.py
+++ server.py
@@ -5,6 +5,8 @@ import google.generativeai as genai
 import faiss
 import numpy as np
 
+TOP_K = 4
+
 load_dotenv()
 
 GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
@@ -32,3 +34,13 @@ def embed_text(text):
     return np.array([e["embedding"] for e in result["embedding"]], dtype="float32")
 
 employee_embeddings = embed_text(employee_chunks)
+
+dimension = employee_embeddings.shape[1]
+index = faiss.IndexFlatL2(dimension)
+index.add(employee_embeddings)
+
+def retrieve_context(query, top_k = TOP_K):
+    query_vector = embed_text(query)
+    distances, indices = index.search(query_vector, top_k)
+    results = [employee_chunks[i] for i in indices[0]]
+    return {"documents": results}
